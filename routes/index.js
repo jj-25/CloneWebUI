7738e19a90cb966a69b636cf1c4e8e4ff43a435f
gg <f2e.george@gmail.com>
2023-10-06 23:24:48 +0800

    feat: parse bundle.css.

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,37 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const router = express.Router();
 
+const CSSRegex = /\.+(css)/;
+const StyleSheetRegex = /stylesheet/;
+
 async function getHtmlAndCss(url) {
   try {
     const response = await axios.get(url);
-    const html = response.data.toLowerCase();
-    const $ = cheerio.load(html.toLowerCase());
-    const css = $("style").html();
 
-    return { html, css };
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const css = $("style").html() ?? "";
+
+    let links;
+    let stylesheet;
+
+    $("link").each((i, el) => {
+      const linkHref = $(el).attr("href");
+      const linkRel = $(el).attr("rel");
+      // find <link rel=stylesheet href="xxx.css"/>
+      if (linkRel.match(StyleSheetRegex) && linkHref.match(CSSRegex)) {
+        links = "https://" + response.request.socket._host + linkHref;
+      }
+    });
+
+    if (links) {
+      stylesheet = await axios.get(links);
+    }
+
+    return {
+      html: html.replace(/<link[^>]*>/g, ""), //trim link tag
+      css: css + stylesheet?.data,
+    };
   } catch (error) {
     console.error(error);
     return null;
